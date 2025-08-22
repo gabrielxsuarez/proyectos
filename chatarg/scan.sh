db50470739521b262e219a13b61054285d929286
Gabriel Suarez <gabrielxsuarez@gmail.com>
2025-08-22 19:35:34 -0300

    .

--- chatarg/scan.sh
+++ chatarg/scan.sh
@@ -1,5 +1,5 @@
-echo -e  "wss\n" && sudo nmap -sS -p- --min-rate 1000 -T4  wss.dalechatea.me > scan_wss.txt
-echo -e "ws01\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws01.dalechatea.me > scan_ws01.txt
-echo -e "ws02\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws02.dalechatea.me > scan_ws02.txt
-echo -e "ws03\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws03.dalechatea.me > scan_ws03.txt
-echo -e "ws04\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws04.dalechatea.me > scan_ws04.txt
+echo  "wss\n" && sudo nmap -sS -p- --min-rate 1000 -T4  wss.dalechatea.me > scan_wss.txt
+echo "ws01\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws01.dalechatea.me > scan_ws01.txt
+echo "ws02\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws02.dalechatea.me > scan_ws02.txt
+echo "ws03\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws03.dalechatea.me > scan_ws03.txt
+echo "ws04\n" && sudo nmap -sS -p- --min-rate 1000 -T4 ws04.dalechatea.me > scan_ws04.txt
